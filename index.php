4bbaa3436df1bf0bf0489434de06d8793541f5aa
Vibhuti Talekar <79747578+vibhuti-talekar@users.noreply.github.com>
2022-02-21 11:21:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasks Go</title>
+
+    <!--icon link-->
+    <link rel="icon" href="assets\icon.png" type="image/x-icon">
+
+    <!-- css link -->
+    <link rel="stylesheet" href="css/style1.css">
+
+    
+<!-- Link Swiper's CSS -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+<!-- Swiper JS -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+
+</head>
+<body>
+   <!--header-->
+   <header>
+   <div class="logo">
+        <a href="#" >Tasks<span>Go</span><img src="assets\icon.png" style="max-height: 28px; margin-left:0px; "></a>   
+    </div>
+   <nav class="navbar">
+       <a href="#home">Home</a>
+       <a href="#features">Features</a>
+       <a href="login.php">Login</a>
+   </nav>
+   </header>
+   <!--header ends-->
+   <!--home-->
+    <section class="home" id="home">
+        <div class="content">
+            <h3><span class="auto-input"></span> <img src="assets\icon.png" style="max-height: 55px; margin-left:1px; ">
+             </h3>
+            <p>TasksGo helps you stay on top of all of your commitments and visualize progress 
+                as priorities change by organizing your tasks. For personal use, you can create 
+                a basic to-do list features to keep on top of things, but if you're using the site 
+                for professional purposes, it's a whole other ball game. One can track progress on 
+                certain work, note on where effort is spent, update status, prioritize tasks and 
+                create planning timelines.</p>
+            <a href="login.php" class="btn">Start Here</a>
+        </div>
+
+        <div class="image">
+            <img src="assets\task.png" alt="" style="width:97%;">
+        </div>
+    </section>
+   
+   <!--home ends-->
+
+
+  
+
+<!--features-->
+<section class="features" id="features">
+    <h1 class="heading">Features</h1>
+
+    <div class="container">
+
+        <div class="box">
+            <img src="assets\lists.png" alt="" >
+            <h3>Create lists</h3>
+            <p>Use the Create list feature to help you list down the number of tasks or activities you have planned for a cetain duration of time. </p>
+            <a href="login.php" class="btn">Read More</a>
+        </div>
+
+        <div class="box">
+            <img src="assets\daily-tasks.png" alt="" >
+            <h3>Create Tasks</h3>
+            <p>Use the Create tasks feature to help you list down the each tasks you have to complete in a certain duration of time.</p>         
+            <a href="login.php" class="btn">Read More</a>
+        </div>
+
+        <div class="box">
+            <img src="assets\deleted.png" alt="" >
+            <h3>Delete Whenever</h3>
+            <p>Delete tasks when you have succesfully completed them. You can also delete tasks if theyre no longer relevant</p>           
+            <a href="login.php" class="btn">Read More</a>
+        </div>
+    </div>
+
+</section>
+<!--features-->
+
+<!--about section-->
+
+<section class="about" id="about">
+<h1 class="heading">About Us</h1>
+<div class="column-about">
+    <div class="image">
+        <img src="assets\main.png" alt="" >
+    </div>
+
+
+    <div class="content">
+        <h3>How does TasksGo help you?</h3>
+        <p>Do you often feel overwhelmed by the amount of work you have to do? Do you find yourself missing deadlines? 
+           Or do you sometimes just forget to do something important, so that people have to chase you to get work done?</p>
+        <p>This site was created to help you manage your time and complete your goals. 
+           By keeping such a list, you make sure that your tasks are written down all in one place so you don't forget 
+           anything important.</p>
+        <div class="buttons">
+            <a href="login.php" class="btn">Login</a>
+        </div>
+    </div>
+
+</div>
+</section>
+
+<!--about section-->
+
+<!-- review addedd here-->
+<h1 class="heading">Reviews</h1>
+<!-- Swiper -->
+<div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\man.png" alt="">
+                <h3 style="color:aqua">John Wilkinson</h3>
+                <div class="comment">
+                     TasksGo is an amazing site that has helped me complete all my work within the required time!
+                 I highly recommend this site if you have a problem with time management.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\woman.png" alt="">
+                <h3 style="color:aqua">Alisha Andrade</h3>
+                <div class="comment">
+                    This has truly been a game changer for me. Ever since I have started using TasksGo, 
+                I have managed to find a lot of free time to do my other hobbies.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\india.png" alt="">
+                <h3 style="color:aqua">Marta Cook</h3>
+                <h3></h3>
+                <div class="comment">
+                     Thank you TasksGo for helping me with listing my work and not forgetting the not-so important work. 
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\man (1).png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\woman (1).png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\business-man.png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\avatar.png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\flight-attendant.png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+        <div class="swiper-slide"><div class="box">
+            <div class="user">
+                <img src="assets\male-teacher.png" alt="">
+                <h3 style="color:aqua">John Doe</h3>
+                <div class="comment">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nisi quo quis ad in alias
+                    consequuntur quaerat harum illo. Quibusdam.
+                </div>
+            </div>
+        </div></div>
+    </div>
+    <div class="swiper-pagination"></div>
+</div>
+<!-- review ended here-->
+
+<!--footer-->
+
+<div class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>About us</h3>
+            <p>This website is created by 4 student from Don Bosco College of Engineering, Fatorda, Goa. 
+                After facing issues with finishing assignments and projects on deadline, a decision was made to
+                create a website that helps with the afore mentioned problem. 
+                This site initially made to help students with college work, now also helps adults with day to day tasks and chores</p>
+        </div>
+
+        <div class="box">
+            <h3>Members</h3>
+            <a href="">Vibhuti talekar</a>
+            <br>
+            <a href="">Ruben Fernandes</a><br>
+            <a href="">Sanresh Mapsekar</a><br>
+            <a href="">Hayden Fernandes</a><br>
+            
+        </div>
+
+        <div class="box">
+            <h3>Links</h3>
+            <a href=""><img src="assets/gith.png" alt="" style="width:16%;"></a>
+        
+        </div>
+    </div>
+</div>
+
+
+<!--footer-->
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+  <script>
+    let typed = new Typed(".auto-input", {
+      strings: ["Your", "Own", "Tasks At A Go"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true
+    })
+  </script>
+
+<script>
+
+       var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+</script>
+</html>
+
